61ac343058d2ec6360b40e463e97970922215e6d
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-17 13:49:00 +0200

    Refactored Component Test
    deleted renderComponent function inside test cases
    added a beforeEach to rerender component before each test

--- test/components/commentBostTest.js
+++ test/components/commentBostTest.js
@@ -1,33 +1,26 @@
 import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/CommentBox';
 
-describe('CommentBox', () =>{
-  it('has a the correct class', (done) => {
-    const commentBox = renderComponent(CommentBox);
+describe('CommentBox', () => {
+  let commentBox;
 
+  beforeEach(() => {
+    commentBox = renderComponent(CommentBox);
+  });
+
+  it('has a the correct class', () => {
     expect(commentBox).to.have.class('comment-box');
-    done();
   });
 
-  it('has a header', (done) => {
-    const commentBox = renderComponent(CommentBox);
-
+  it('has a header', () => {
     expect(commentBox.find('h5')).to.exist;
-    done();
   });
 
-  it('has text form', (done) => {
-    const commentBox = renderComponent(CommentBox);
-
+  it('has text form', () => {
     expect(commentBox.find('textarea')).to.exist;
-    done();
   });
 
-  it('has a submit button', (done) => {
-    const commentBox = renderComponent(CommentBox);
-
+  it('has a submit button', () => {
     expect(commentBox.find('button')).to.exist;
-    done();
   });
-
 });
